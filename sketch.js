b1c19d3b33ec6b20f52c7e4fce61a5c5531662da
vanshajgandhir <vanshajgandhir998@gmail.com>
2020-07-16 21:07:18 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,6 +15,8 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
+    platform = new Ground(200,300,400,200);
 
     
     ground = new Ground(600,height,1200,20)
@@ -45,9 +48,11 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+    platform.display();
     ground.display();
     pig1.display();
     log1.display();
+    
 
     box3.display();
     box4.display();
